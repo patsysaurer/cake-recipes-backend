40e41c517523164100061784cf54656962dfb0ba
Patsy <patsyesaurer@gmail.com>
2024-01-27 22:17:46 -0800

    API endpoints and validations complete

--- app/models/cake.rb
+++ app/models/cake.rb
@@ -1,3 +1,4 @@
 class Cake < ApplicationRecord
     belongs_to :user
+    validates :cakeName, presence: true
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -11,7 +11,7 @@ user1_cakes = [
         cakeIngredients:"170 g Honey, 8 g Vanilla, 4 Eggs, 300 g Buttermilk, 168 g Sugar, 226 g Butter, 400 g AP Flour, 7 g Baking Powder, 5 g Salt, 1 tsp Cinnamon, 1/4 tsp Nutmeg, 1/8 tsp Ginger, 1/8 tsp Cloves",
         cakeInstructions:"1. Preheat the oven to 350°F. 2. Line two 9” cake pans with parchment paper. 3. In a large bowl, sift together the dry ingredients. 4. In a bowl of a stand mixer fitted with a paddle attachment, cream together the butter and sugar. 5. Then add in the eggs, honey, and vanilla and combine. 6. Alternate the dry ingredients with the buttermilk until combined. 7. Pour the batter evenly into the prepared cake pans. 8. Bake for 30 minutes or until a cake pin comes out clean. 9. Cool on a wire rack for about 15 minutes before removing.",
         cakeServings: 12,
-        cakeImage:"https://res.cloudinary.com/dl11m63ow/image/upload/w_1000,ar_1:1,c_fill,g_auto,e_art:hokusai/v1706415571/honey-cake_hcmhqo.jpg",
+        cakeImage:"https://res.cloudinary.com/dl11m63ow/image/upload/w_1000,ar_1:1,c_fill,g_auto,e_art:hokusai/v1706415571/honey-cake_hcmhqo.jpg"
     },
     {
         cakeName: "Birthday Cake",
@@ -19,7 +19,7 @@ user1_cakes = [
         cakeIngredients:"350 g AP flour, 8 g Baking powder, 3.7 g Salt, 227 g Butter, softened, 417 g Sugar, 5 Eggs, 2 tsp Vanilla extract, 270 ml	Buttermilk",
         cakeInstructions:"1. Preheat the oven to 375°F. 2. Grease and line two 9” cake pans with parchment paper. 3. In a stand mixer fitted with a paddle attachment, cream together the butter and sugar until fluffy. 4. Scrape down the sides and beat in the eggs one at a time until incorporated, then add in vanilla. 5. With the mixer on the lowest speed, gradually add the dry ingredients alternating with the buttermilk and mix until smooth and creamy. 6. Pour the batter into the prepared pans, making sure to scale each one evenly. 7. Bake for 28-30 minutes or until the cake tester comes out clean. Allow the cakes to rest in the pans for 10 minutes. 8. Run a knife around the edges to loosen the cake and invert it onto a cooling rack. 9. Let the cakes cool completely before slicing and filling.",
         cakeServings: 12,
-        cakeImage:"https://res.cloudinary.com/dl11m63ow/image/upload/w_1000,ar_1:1,c_fill,g_auto,e_art:hokusai/v1706416608/bday-cake_u6ivhn.jpg",
+        cakeImage:"https://res.cloudinary.com/dl11m63ow/image/upload/w_1000,ar_1:1,c_fill,g_auto,e_art:hokusai/v1706416608/bday-cake_u6ivhn.jpg"
     }
 ]
 
