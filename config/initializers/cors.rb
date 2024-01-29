6f3c0ca843ad63d71a452fe079f47b11b88bba8c
Patsy <patsyesaurer@gmail.com>
2024-01-29 02:05:21 -0800

    changed backend url

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,7 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3001'
+    origins 'cake-recipes.onrender.com'
+    # origins 'http://localhost:3001'
     resource '*',
     headers: ["Authorization"],
     expose: ["Authorization"],
